97e40b7f1d0e70da28d35e103f46e9623095e3d4
JohnMejia321 <64913832+JohnMejia321@users.noreply.github.com>
2021-06-03 10:51:42 -0500

    Add files via upload

---
+++ ApiRest.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+personas = []
+
+class Personas(Resource):
+        def get(self, valor):
+            for persona in personas:
+                if persona == valor:
+                    return {'persona buscada = ': persona}
+            return {'resultado': 'Persona no encontrada'}
+
+        def post(self, valor):
+            persona = valor
+            personas.append(persona)
+            return {'resultado': 'Persona añadidad correctamente'}
+
+        def delete(self, valor):
+            for indice, persona in enumerate(personas):
+                if persona == valor:
+                    personas.pop(indice)
+                    return {'resultado': 'Persona borrada correctamente'}
+
+class Listar(Resource):
+        def get(self):
+            return {'resultado': personas}
+
+api.add_resource(Personas, '/persona/<string:valor>')
+api.add_resource(Listar, '/listar')
+
+if __name__ == '__main__':
+        app.run(debug=True)
